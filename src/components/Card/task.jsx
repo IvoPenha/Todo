3dfbe431d966f7456c2e4e3abacba337017f5ef6
Ivo Penha <pedroivoxps@gmail.com>
2022-08-15 07:42:29 -0300

    updating

--- src/components/Card/task.jsx
+++ src/components/Card/task.jsx
@@ -1,5 +1,16 @@
 import Card from 'react-bootstrap/Card';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import './task.css'
+import  styled  from 'styled-components';
+const StyledFormatListBulletedIcon = styled(FormatListBulletedIcon)`
+  color:#C2C2C2;
+
+`;
+const StyledAccessTimeIcon = styled(AccessTimeIcon)`
+  color:#C2C2C2;
+`;
+
 export function Task(){
     return(
        
@@ -15,7 +26,7 @@ export function Task(){
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
-      <Card.Link href="#" className='link'>Card Link</Card.Link>
+      <Card.Link className='footer'> <StyledFormatListBulletedIcon className='Todos'></StyledFormatListBulletedIcon  > <span> Tarefas </span>  <StyledAccessTimeIcon className='Hour'/> <span> 00:00:00  </span> </Card.Link>
     </Card.Body>
   </Card>
        
